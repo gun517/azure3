7863e176bed61083e04ea5365c6897dac70bc70b
khun <rigel517@gmail.com>
2016-04-18 01:42:43 +0900

    second

--- azure3/urls.py
+++ azure3/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
+from blog import views
 
 def hello(request):
     return HttpResponse('''
@@ -27,8 +28,8 @@ def hello(request):
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$' , hello),
-    url(r'^blog/$', 'blog.views.post_list'),
-    url(r'^blog/(?P<pk>\d+)/$', 'blog.views.post_detail'),
+    url(r'^blog/$', views.post_list),
+    url(r'^blog/(?P<pk>\d+)/$', views.post_detail),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
